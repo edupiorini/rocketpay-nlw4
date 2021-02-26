57326625a7fdc73950b7f4499fb3cfc599a703fa
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-02-26 07:55:10 -0300

    accounts now are related to users

--- priv/repo/migrations/20210226094504_create_accounts_table.exs
+++ priv/repo/migrations/20210226094504_create_accounts_table.exs
@@ -2,7 +2,7 @@ defmodule Rocketpay.Repo.Migrations.CreateAccountsTable do
   use Ecto.Migration
 
   def change do
-    create table: accounts do
+    create table :accounts do
       add :balance, :decimal #Utilizamos decimal especificamente para valores monetários
       add :user_id, references(:users, type: :binary_id)
 
